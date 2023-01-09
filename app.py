ee5b635e5e5a16fda0e5def82c34fbba14a4b881
Óscar Lucas <98739924+oscarlucas22@users.noreply.github.com>
2023-01-09 10:42:17 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from funciones import *
+from flask import Flask, render_template, abort, request
+import pymysql
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/inicio', methods=["POST"])
+def inicio():
+    usuario=request.form.get("usuario")
+    contrasena=request.form.get("contraseña")
+    maquina1='localhost'
+    nombredb1='GN'
+    conex= pymysql.connect(host=maquina1,user=usuario,password=contrasena,database=nombredb1)
+    datos=Mostrar_Profesores(conex)
+    return render_template("inicio.html",datos=datos)
+
+app.run('0.0.0.0', 5000, debug=False)
+    
